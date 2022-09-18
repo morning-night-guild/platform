962be34b0493370c1a75cad0bc9f5b8e5a6c8b9c
takokun778 <takokun.778@gmail.com>
2022-09-18 15:27:07 +0900

    test(#3) 記事共有UseCaseのテストを修正

--- backend/usecase/interactor/article/share_test.go
+++ backend/usecase/interactor/article/share_test.go
@@ -13,7 +13,10 @@ import (
 	"github.com/morning-night-guild/platform/usecase/repository"
 )
 
-var _ repository.Article = (*ArticleMock)(nil)
+var (
+	_ repository.Article = (*ArticleMock)(nil)
+	_ repository.OGP     = (*OGPMock)(nil)
+)
 
 // Article 記事リポジトリのモック.
 type ArticleMock struct {
@@ -25,11 +28,29 @@ func (a *ArticleMock) Save(ctx context.Context, article model.Article) error {
 	return a.Err
 }
 
+// OGP OGPリポジトリのモック.
+type OGPMock struct {
+	Article model.Article
+	Err     error
+}
+
+// Create 記事を作成するモックメソッド.
+func (o *OGPMock) Create(ctx context.Context, url article.URL) (model.Article, error) {
+	if o.Err != nil {
+		return model.Article{}, o.Err
+	}
+
+	o.Article.URL = url
+
+	return o.Article, nil
+}
+
 func TestShareInteractorExecute(t *testing.T) {
 	t.Parallel()
 
 	type fields struct {
 		articleRepository repository.Article
+		ogpRepository     repository.OGP
 	}
 
 	type args struct {
@@ -50,30 +71,63 @@ func TestShareInteractorExecute(t *testing.T) {
 				articleRepository: &ArticleMock{
 					Err: nil,
 				},
+				ogpRepository: &OGPMock{
+					Article: model.CreateArticle(
+						article.Title("タイトル"),
+						article.URL("https://example.com"),
+						article.Thumbnail("https://example.com/image"),
+						article.TagList{},
+					),
+					Err: nil,
+				},
 			},
 			args: args{
 				ctx: context.Background(),
 				input: port.ShareArticleInput{
-					Title:     article.Title("タイトル"),
-					URL:       article.URL("https://example.com/image"),
-					Thumbnail: article.Thumbnail("https://example.com/image"),
+					URL: article.URL("https://example.com"),
 				},
 			},
 			wantErr: false,
 		},
 		{
-			name: "Repositoryのerrorを握りつぶさない",
+			name: "記事Repositoryのerrorを握りつぶさない",
 			fields: fields{
 				articleRepository: &ArticleMock{
-					Err: errors.New("repository error"),
+					Err: errors.New("article repository error"),
+				},
+				ogpRepository: &OGPMock{
+					Article: model.CreateArticle(
+						article.Title("タイトル"),
+						article.URL("https://example.com"),
+						article.Thumbnail("https://example.com/image"),
+						article.TagList{},
+					),
+					Err: nil,
 				},
 			},
 			args: args{
 				ctx: context.Background(),
 				input: port.ShareArticleInput{
-					Title:     article.Title("タイトル"),
-					URL:       article.URL("https://example.com/image"),
-					Thumbnail: article.Thumbnail("https://example.com/image"),
+					URL: article.URL("https://example.com"),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "OGPRepositoryのerrorを握りつぶさない",
+			fields: fields{
+				articleRepository: &ArticleMock{
+					Err: nil,
+				},
+				ogpRepository: &OGPMock{
+					Article: model.Article{},
+					Err:     errors.New("ogp repository error"),
+				},
+			},
+			args: args{
+				ctx: context.Background(),
+				input: port.ShareArticleInput{
+					URL: article.URL("https://example.com"),
 				},
 			},
 			wantErr: true,
@@ -84,7 +138,7 @@ func TestShareInteractorExecute(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			s := interactor.NewShareInteractor(tt.fields.articleRepository)
+			s := interactor.NewShareInteractor(tt.fields.articleRepository, tt.fields.ogpRepository)
 			got, err := s.Execute(tt.args.ctx, tt.args.input)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ShareInteractor.Execute() error = %v, wantErr %v", err, tt.wantErr)
