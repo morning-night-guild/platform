35d8cea5295fd01592f5d5588fa95e4eb519ef85
takokun778 <takokun.778@gmail.com>
2022-10-17 17:01:04 +0900

    feat(#43) タグ一覧取得コントローラーを実装

--- backend/app/core/adapter/controller/article_test.go
+++ backend/app/core/adapter/controller/article_test.go
@@ -5,11 +5,13 @@ import (
 	"errors"
 	"os"
 	"reflect"
-	"strconv"
 	"testing"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/google/uuid"
 	"github.com/morning-night-guild/platform/app/core/adapter/controller"
+	"github.com/morning-night-guild/platform/app/core/model"
+	"github.com/morning-night-guild/platform/app/core/model/article"
 	me "github.com/morning-night-guild/platform/app/core/model/errors"
 	"github.com/morning-night-guild/platform/app/core/usecase"
 	"github.com/morning-night-guild/platform/app/core/usecase/port"
@@ -17,17 +19,22 @@ import (
 )
 
 type ShareMock struct {
-	err error
-}
-
-func NewShareMock(err error) ShareMock {
-	return ShareMock{
-		err: err,
-	}
+	Err error
 }
 
 func (s ShareMock) Execute(ctx context.Context, input port.ShareArticleInput) (port.ShareArticleOutput, error) {
-	return port.ShareArticleOutput{}, s.err
+	return port.ShareArticleOutput{}, s.Err
+}
+
+type ListMock struct {
+	Articles []model.Article
+	Err      error
+}
+
+func (l ListMock) Execute(ctx context.Context, input port.ListArticleInput) (port.ListArticleOutput, error) {
+	return port.ListArticleOutput{
+		Articles: l.Articles,
+	}, l.Err
 }
 
 func TestArticleShare(t *testing.T) {
@@ -38,6 +45,7 @@ func TestArticleShare(t *testing.T) {
 	type fields struct {
 		apiKey string
 		share  usecase.Usecase[port.ShareArticleInput, port.ShareArticleOutput]
+		list   usecase.Usecase[port.ListArticleInput, port.ListArticleOutput]
 	}
 
 	type args struct {
@@ -56,7 +64,10 @@ func TestArticleShare(t *testing.T) {
 			name: "記事の共有ができる",
 			fields: fields{
 				apiKey: "test",
-				share:  NewShareMock(nil),
+				share: ShareMock{
+					Err: nil,
+				},
+				list: ListMock{},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -73,7 +84,10 @@ func TestArticleShare(t *testing.T) {
 			name: "X-API-KEYが不正の時、認証エラーになる",
 			fields: fields{
 				apiKey: "invalid-api-key",
-				share:  NewShareMock(nil),
+				share: ShareMock{
+					Err: nil,
+				},
+				list: ListMock{},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -90,7 +104,10 @@ func TestArticleShare(t *testing.T) {
 			name: "URLが不正の時、バッドリクエストエラーになる",
 			fields: fields{
 				apiKey: "test",
-				share:  NewShareMock(nil),
+				share: ShareMock{
+					Err: nil,
+				},
+				list: ListMock{},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -107,9 +124,10 @@ func TestArticleShare(t *testing.T) {
 			name: "ユースケースでバリデーションエラーが発生した際、バッドリクエストエラーになる",
 			fields: fields{
 				apiKey: "test",
-				share: NewShareMock(
-					me.NewValidationError("validation error"),
-				),
+				share: ShareMock{
+					Err: me.NewValidationError("validation error"),
+				},
+				list: ListMock{},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -126,9 +144,9 @@ func TestArticleShare(t *testing.T) {
 			name: "ユースケースでバリデーションエラー以外のエラーが発生した際、サーバーエラーになる",
 			fields: fields{
 				apiKey: "test",
-				share: NewShareMock(
-					errors.New("unknown error"),
-				),
+				share: ShareMock{
+					Err: errors.New("unknown error"),
+				},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -147,7 +165,7 @@ func TestArticleShare(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			a := controller.NewArticle(tt.fields.share)
+			a := controller.NewArticle(tt.fields.share, tt.fields.list)
 			tt.args.req.Header().Set("X-API-KEY", tt.fields.apiKey)
 			got, err := a.Share(tt.args.ctx, tt.args.req)
 			if err != nil && err != tt.wantErr {
@@ -166,13 +184,16 @@ func TestArticleList(t *testing.T) {
 	t.Parallel()
 
 	type fields struct {
-		share port.ShareArticle
+		share usecase.Usecase[port.ShareArticleInput, port.ShareArticleOutput]
+		list  usecase.Usecase[port.ListArticleInput, port.ListArticleOutput]
 	}
 
 	type args struct {
 		ctx context.Context
 		req *connect.Request[articlev1.ListRequest]
 	}
+
+	id := uuid.New()
 
 	tests := []struct {
 		name    string
@@ -182,9 +203,21 @@ func TestArticleList(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "記事の一覧が取得できる",
+			name: "記事の一覧が取得できる（ネクストトークンあり）",
 			fields: fields{
-				share: NewShareMock(nil),
+				share: ShareMock{},
+				list: ListMock{
+					Articles: []model.Article{
+						{
+							ID:          article.ID(id),
+							Title:       article.Title("title"),
+							URL:         article.URL("https://example.com"),
+							Description: article.Description("description"),
+							Thumbnail:   article.Thumbnail("https://example.com"),
+							TagList:     article.TagList{},
+						},
+					},
+				},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -198,17 +231,76 @@ func TestArticleList(t *testing.T) {
 			want: connect.NewResponse(&articlev1.ListResponse{
 				Articles: []*articlev1.Article{
 					{
-						Id:          strconv.Itoa(0),
-						Title:       "example",
+						Id:          id.String(),
+						Title:       "title",
 						Url:         "https://example.com",
-						Description: "example description",
-						Thumbnail:   "",
-						Tags:        []string{"Go"},
+						Description: "description",
+						Thumbnail:   "https://example.com",
+						Tags:        []string{},
+					},
+				},
+				NextPageToken: "MQ==",
+			}),
+			wantErr: false,
+		},
+		{
+			name: "記事の一覧が取得できる（ネクストトークンなし）",
+			fields: fields{
+				share: ShareMock{},
+				list: ListMock{
+					Articles: []model.Article{
+						{
+							ID:          article.ID(id),
+							Title:       article.Title("title"),
+							URL:         article.URL("https://example.com"),
+							Description: article.Description("description"),
+							Thumbnail:   article.Thumbnail("https://example.com"),
+							TagList:     article.TagList{},
+						},
+					},
+				},
+			},
+			args: args{
+				ctx: context.Background(),
+				req: &connect.Request[articlev1.ListRequest]{
+					Msg: &articlev1.ListRequest{
+						PageToken:   "",
+						MaxPageSize: 3,
+					},
+				},
+			},
+			want: connect.NewResponse(&articlev1.ListResponse{
+				Articles: []*articlev1.Article{
+					{
+						Id:          id.String(),
+						Title:       "title",
+						Url:         "https://example.com",
+						Description: "description",
+						Thumbnail:   "https://example.com",
+						Tags:        []string{},
 					},
 				},
 				NextPageToken: "",
 			}),
 			wantErr: false,
+		},
+		{
+			name: "不正なサイズを指定して記事の一覧が取得できない",
+			fields: fields{
+				share: ShareMock{},
+				list:  ListMock{},
+			},
+			args: args{
+				ctx: context.Background(),
+				req: &connect.Request[articlev1.ListRequest]{
+					Msg: &articlev1.ListRequest{
+						PageToken:   "",
+						MaxPageSize: 1000,
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
 		},
 	}
 
@@ -216,7 +308,7 @@ func TestArticleList(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			a := controller.NewArticle(tt.fields.share)
+			a := controller.NewArticle(tt.fields.share, tt.fields.list)
 			got, err := a.List(tt.args.ctx, tt.args.req)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Article.List() error = %v, wantErr %v", err, tt.wantErr)
