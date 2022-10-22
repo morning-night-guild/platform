6679d9e6940905f2837f6bf2e6724ea720a11fa5
Shitomo <cadaeibf@outlook.com>
2022-10-22 20:12:02 +0900

    chore(#63) 必要なコマンドをインストールするよう修正

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "Hello world"
-
 export PROTOC_VERSION=v3.20.2
+
+yum update
+
+yum install wget npm yarn
 
 wget https://github.com/protocolbuffers/protobuf/releases/download/${PROTOC_VERSION}/protoc-$(echo ${PROTOC_VERSION} | sed 's/v//')-linux-x86_64.zip -O protobuf.zip && \
     unzip protobuf.zip -d /usr/local/bin/protobuf && \
