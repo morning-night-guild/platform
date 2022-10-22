35302291911dfe9d3d72e6b5c66ab2873810998a
Shitomo <cadaeibf@outlook.com>
2022-10-22 19:59:57 +0900

    chore(#63) frontendのデプロイスクリプトを整備

---
+++ frontend/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PROTOC_VERSION=v3.20.2
+
+wget https://github.com/protocolbuffers/protobuf/releases/download/${PROTOC_VERSION}/protoc-$(echo ${PROTOC_VERSION} | sed 's/v//')-linux-x86_64.zip -O protobuf.zip && \
+    unzip protobuf.zip -d /usr/local/bin/protobuf && \
+    rm protobuf.zip && \
+    chmod -R 755 /usr/local/bin/protobuf/*
+
+export PATH=PATH:/usr/local/bin/protobuf/
+
+(cd ../api && npm install && buf generage --template buf.frontend.gen.yaml)
+
+yarn build
