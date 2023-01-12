950cc397eb7495a1f1f1e86c9200c44793f337ea
Shitomo <cadaeibf@outlook.com>
2023-01-13 00:49:12 +0900

    fix(#253) 余計な@を削除

--- supabase/functions/slack/index.ts
+++ supabase/functions/slack/index.ts
@@ -1,4 +1,4 @@
-import { DOMParser, Element, NodeType } from "https://deno.land/x/deno_dom@/deno-dom-wasm.ts";
+import { DOMParser, Element, NodeType } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 
 type OGP = {
